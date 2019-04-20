d4cb163ad421816f3fc5594674088acfdfc1419f
Ryan Rhodes <ryan@ryanrhodes.com>
2019-04-19 21:05:14 -0400

    adding some comments

--- bitcoin-chart.js
+++ bitcoin-chart.js
@@ -10,12 +10,17 @@ imgur.setClientId('947ba3967a16848')
 require ('ansicolor').nice
 
 exports.chart = async function (timeRange = '15m') {
+    // retrieve pricing data
     const index = 4 // [ timestamp, open, high, low, close, volume ]
     const ohlcv = await new ccxt.okcoinusd ().fetchOHLCV ('BTC/USD', timeRange)
     const lastPrice = ohlcv[ohlcv.length - 1][index] // closing price
     const series = ohlcv.map (x => x[index])         // closing price
     const bitcoinRate = ('₿ = $' + lastPrice).green
+
+    // generate ascii plot of pricing data
     const chart = asciichart.plot (series, { height: 15, offset: 2, padding: '' })
+    
+    // log chart
     log.yellow ("\n" + chart, bitcoinRate, "\n")
     
     const pngConfig = {
@@ -27,8 +32,10 @@ exports.chart = async function (timeRange = '15m') {
         output: 'buffer'
     }
 
+    // convert ascii chart to canvas, then a png
     const buffer = await text2png(chart + "\n\nBTC = $" + lastPrice, pngConfig);
     
+    // upload png to imgur, and return link
     return imgur.uploadBase64(buffer.toString('base64'))
         .then(function (json) {
             return json.data.link;
